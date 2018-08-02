2441034ad8e709367165d93a866055a0d129a1dc
Dave Laing <dave.laing.80@gmail.com>
2018-08-02 15:49:24 +1000

    Some nix changes to show off a trick

--- default.nix
+++ default.nix
@@ -1,14 +1,17 @@
 { reflex-platform ? import ./nix/reflex-platform.nix
 , compiler   ? "ghc"
+, local-basic-host ? false
 } :
 let
 
   pkgs = reflex-platform.nixpkgs.pkgs;
   ghc = reflex-platform.${compiler};
 
+  reflex-basic-host-sources = if local-basic-host then ../reflex-basic-host else (import ./nix/reflex-basic-host.nix);
+
   modifiedHaskellPackages = ghc.override {
     overrides = self: super: {
-      reflex-basic-host = self.callPackage (import ./nix/reflex-basic-host.nix) {};
+      reflex-basic-host = self.callPackage reflex-basic-host-sources {};
     };
   };
 
--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { nixpkgs ? import <nixpkgs> {}
 , compiler ? "ghc"
+, local-basic-host ? true
 } : 
 let
   inherit (nixpkgs) pkgs;
   reflex-platform = import ./nix/reflex-platform.nix;
-  drv = import ./. { inherit reflex-platform compiler; };
+  drv = import ./. { inherit reflex-platform compiler local-basic-host; };
 in
   if pkgs.lib.inNixShell then drv.env else drv
