baee7da4c19731fb784b3352f4560733b1ddd125
Jack Kelly <jack.kelly@data61.csiro.au>
2019-05-17 16:01:38 +1000

    Use a nix-shell to get nix-prefetch-git

--- nix/update.sh
+++ nix/update.sh
@@ -1,4 +1,5 @@
-#! /usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nix-prefetch-git
 
 nix-prefetch-git https://github.com/reflex-frp/reflex-platform > reflex-platform.json
 nix-prefetch-git https://github.com/dalaing/reflex-basic-host > reflex-basic-host.json
