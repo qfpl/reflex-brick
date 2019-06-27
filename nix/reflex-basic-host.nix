dfc2a43fdc5f4e6d2a81b76c0aaee1dcca673cd4
Jack Kelly <jack@jackkelly.name>
2019-06-27 22:46:39 +1000

    Update deps

--- nix/reflex-basic-host.nix
+++ nix/reflex-basic-host.nix
@@ -1,14 +1,13 @@
-let  
+let
   initialNixpkgs = import <nixpkgs> {};
 
   sources = rec {
     reflex-basic-host-info-pinned = initialNixpkgs.pkgs.lib.importJSON ./reflex-basic-host.json;
     reflex-basic-host = initialNixpkgs.pkgs.fetchFromGitHub {
-      owner = "dalaing";
+      owner = "qfpl";
       repo = "reflex-basic-host";
       inherit (reflex-basic-host-info-pinned) rev sha256;
     };
   };
 in
   sources.reflex-basic-host
-
--- nix/update.sh
+++ nix/update.sh
@@ -2,4 +2,4 @@
 #! nix-shell -i bash -p nix-prefetch-git
 
 nix-prefetch-git https://github.com/reflex-frp/reflex-platform > reflex-platform.json
-nix-prefetch-git https://github.com/dalaing/reflex-basic-host > reflex-basic-host.json
+nix-prefetch-git https://github.com/qfpl/reflex-basic-host > reflex-basic-host.json
